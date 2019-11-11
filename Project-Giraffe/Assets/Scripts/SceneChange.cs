7b7e478abc960cf76c332e4e78a8180d9b94d19d
James Li <ryojamesli@gmail.com>
2019-11-12 00:11:17 +0100

    Fading_Anim
    Fade_In/Out Animation for Scenechanges

---
+++ Project-Giraffe/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    protected Animator animator;
+
+
+    private void switchToScene(int id = 1)
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.touchCount > 0) switchToScene();
+    }
+}

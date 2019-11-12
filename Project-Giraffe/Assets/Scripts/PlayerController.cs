07ebb9563f33304a35767b66435e194d79761e30
James Li <ryojamesli@gmail.com>
2019-11-12 18:14:27 +0100

    Movement added
    2D Rigibody, PlayerController added

---
+++ Project-Giraffe/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    protected Rigidbody2D m_RigidBody;
+    protected Vector2 m_Direction;
+    [SerializeField]
+    protected float m_Speed;
+
+    private void Awake()
+    {
+        Input.multiTouchEnabled = true;
+        m_RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0) moveCharacter(Input.GetTouch(0));
+    }
+
+    void moveCharacter(Touch input)
+    {
+        m_Direction = input.position.x < 500 ? Vector2.left : Vector2.right;
+        m_RigidBody.velocity += m_Direction * m_Speed;
+    }
+}

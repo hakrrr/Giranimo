5f42b897701c3694e3f82a55d1e898b239d87769
James Li <ryojamesli@gmail.com>
2019-11-13 09:50:30 +0100

    Consumables added
    Gameobject + Script added

---
+++ Project-Giraffe/Assets/Scripts/Consumable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumable : MonoBehaviour
+{
+    [SerializeField] protected float m_FallSpeed;
+    [SerializeField] protected float m_PushFactor;
+    protected Rigidbody2D m_RigidBody;
+    protected Rigidbody2D m_Player;
+
+    private void Awake()
+    {
+        m_RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        m_RigidBody.velocity = 0.1f * Vector2.down * m_FallSpeed;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            m_Player = collision.gameObject.GetComponent<Rigidbody2D>();
+            m_Player.AddForce(10 * Vector2.up * m_PushFactor);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
--- Project-Giraffe/Assets/Scripts/PlayerController.cs
+++ Project-Giraffe/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] protected float m_Speed;
     protected Rigidbody2D m_RigidBody;
     protected Vector2 m_Direction;
-    [SerializeField]
-    protected float m_Speed;
 
     protected Vector3 m_Velocity = Vector3.zero;
+
     private void Awake()
     {
         Input.multiTouchEnabled = true;
         m_RigidBody = GetComponent<Rigidbody2D>();
     }
-
 
     private void FixedUpdate()
     {
@@ -26,7 +25,6 @@ public class PlayerController : MonoBehaviour
     void moveCharacter(Touch input)
     {
         m_Direction = input.position.x < 500 ? Vector2.left : Vector2.right;
-        //m_RigidBody.velocity = Vector2.Lerp(m_RigidBody.velocity, m_Direction * m_Speed, 0.8f);
         m_RigidBody.velocity = Vector3.SmoothDamp(m_RigidBody.velocity, m_Direction * m_Speed, ref m_Velocity, .1f);
     }
 }
